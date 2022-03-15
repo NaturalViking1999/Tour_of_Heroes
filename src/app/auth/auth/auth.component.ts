e425bfc1f27c19eca2fcbcea6f35596a145bb298
Alexander <warrior0edem@gmail.com>
2022-03-15 17:05:24 +0300

    add auth and pipe 2

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -3,8 +3,59 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  template: `
+  <div>
+    <h2>Create Account</h2>
+    <p class="small">Create a free salmanwap account to access all content.</p>
+    <form [formGroup]="form" (ngSubmit)="authorize()">
+        <mat-form-field class="example-full-width" appearance="fill">
+            <mat-label>Email</mat-label>
+            <input matInput type="text" placeholder="Add your email" formControlName="email">
+            <small></small>
+        </mat-form-field>
+        <mat-form-field class="example-full-width" appearance="fill">
+            <mat-label>Username</mat-label>
+            <input matInput type="text" placeholder="Add your username" formControlName="username">
+            <small></small>
+        </mat-form-field>
+        <mat-form-field class="example-full-width" appearance="fill">
+            <mat-label for="">Password</mat-label>
+            <input matInput type="password" placeholder="Add your password" formControlName="password">
+            <small></small>
+        </mat-form-field>
+        <button mat-raised-button color="primary" type="submit" [disabled]="form.invalid">Create a free account</button>
+    </form>
+</div>`,
+  styles: [`
+  form {
+    display: flex;
+    flex-direction: column;
+    margin: 20px;
+  }
+  
+  div {
+    margin: 0 auto;
+    width: 50vw;
+    background: rgb(255, 255, 255);
+    background: linear-gradient(
+      90deg,
+      rgba(255, 255, 255, 1) 0%,
+      rgba(225, 240, 250, 1) 27%,
+      rgba(206, 231, 247, 1) 44%,
+      rgba(185, 220, 243, 1) 63%,
+      rgba(167, 211, 240, 1) 85%,
+      rgba(144, 200, 236, 1) 100%
+    );
+    border-radius: 20px;
+    padding: 10px;
+    box-shadow: 2px 2px 4px;
+  }
+  
+  h2,
+  .small {
+    text-align: center;
+  }
+  `]
 })
 export class AuthComponent implements OnInit {
   form!: FormGroup;
