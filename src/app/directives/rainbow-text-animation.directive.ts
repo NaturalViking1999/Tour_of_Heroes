35fdbc0a36878c9faa5206d2899deb008502417e
Alexander <warrior0edem@gmail.com>
2022-03-22 18:41:53 +0300

    hate to animate

--- src/app/directives/rainbow-text-animation.directive.ts
+++ src/app/directives/rainbow-text-animation.directive.ts
@@ -4,15 +4,15 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
   selector: '[appRainbowTextAnimation]'
 })
 export class RainbowTextAnimationDirective {
-  
-  constructor( private el: ElementRef ) { }
+
+  // constructor( private el: ElementRef ) { }
 
   @HostListener('mouseenter', ['$event']) onEnter(event: any) {
     if(!event.path[0].classList.contains('anim-text-flow')) {
       event.path[0].classList.add('anim-text-flow');
     }
-    let chars = this.el.nativeElement.textContent.trim('').split("");
-    this.el.nativeElement = '<span>' + chars.join('</span><span>') + '</span>';
+    // let chars = this.el.nativeElement.textContent.split("");
+    // this.el.nativeElement = '<span>' + chars.join('</span><span>') + '</span>';
   }
 
   @HostListener('mouseleave', ['$event']) onLeave(event: any) {
