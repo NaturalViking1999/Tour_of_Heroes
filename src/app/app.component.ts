abd4dc48cfeb0f390e688fd97d07220ee915a320
Alexander <warrior0edem@gmail.com>
2022-03-22 18:15:32 +0300

    hate to animate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
   <div class="container">
   <h1 appTextModificator>{{title | fencecase}}</h1>
   <h3 appTextModificatorHost>My Heroes are the best Heroes!</h3>
-  <p id="animate"><span id="block" appRainbowTextAnimation>
-  <span>A</span><span>N</span><span>I</span><span>M</span><span>A</span><span>T</span><span>I</span><span>O</span><span>N</span>
+  <p id="animate" class="anim-text-flow" appRainbowTextAnimation><span>
+  Animation for Sergey
   </span></p>
  
 <div class="nav">
--- src/app/directives/rainbow-text-animation.directive.ts
+++ src/app/directives/rainbow-text-animation.directive.ts
@@ -4,14 +4,18 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
   selector: '[appRainbowTextAnimation]'
 })
 export class RainbowTextAnimationDirective {
-
+  
   constructor( private el: ElementRef ) { }
 
-  @HostListener('mouseenter') onEnter() {
-    this.el.nativeElement.classList.add('anim-text-flow');
+  @HostListener('mouseenter', ['$event']) onEnter(event: any) {
+    if(!event.path[0].classList.contains('anim-text-flow')) {
+      event.path[0].classList.add('anim-text-flow');
+    }
+    let chars = this.el.nativeElement.textContent.trim('').split("");
+    this.el.nativeElement = '<span>' + chars.join('</span><span>') + '</span>';
   }
 
-  @HostListener('mouseleave') onLeave() {
-    this.el.nativeElement.classList.remove('anim-text-flow')
+  @HostListener('mouseleave', ['$event']) onLeave(event: any) {
+    event.path[0].classList.remove('anim-text-flow')
   }
 }
