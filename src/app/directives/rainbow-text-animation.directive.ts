4d481564861f6c52fb8d19c26217544fd8e688c1
Alexander <warrior0edem@gmail.com>
2022-03-22 22:06:45 +0300

    From the best man to the best man

--- src/app/directives/rainbow-text-animation.directive.ts
+++ src/app/directives/rainbow-text-animation.directive.ts
@@ -4,18 +4,26 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
   selector: '[appRainbowTextAnimation]'
 })
 export class RainbowTextAnimationDirective {
+  private textDefault!: string;
 
-  // constructor( private el: ElementRef ) { }
+  constructor( private el: ElementRef ) { }
 
   @HostListener('mouseenter', ['$event']) onEnter(event: any) {
     if(!event.path[0].classList.contains('anim-text-flow')) {
       event.path[0].classList.add('anim-text-flow');
     }
-    // let chars = this.el.nativeElement.textContent.split("");
-    // this.el.nativeElement = '<span>' + chars.join('</span><span>') + '</span>';
+    this.textDefault = event.path[0].textContent;
+    let span = document.createElement('span');
+    let chars = this.el.nativeElement.textContent.trim().split("");
+    chars.map((item: string) => {
+      span.innerHTML += `<span>${item}</span>`;
+      event.path[0].textContent = null;
+      return this.el.nativeElement.append(span);
+    })
   }
 
   @HostListener('mouseleave', ['$event']) onLeave(event: any) {
-    event.path[0].classList.remove('anim-text-flow')
+    event.path[0].classList.remove('anim-text-flow');
+    event.path[0].textContent = this.textDefault;
   }
 }
