8c6997f974f1d26debf51ff96155463bb0e89fce
Alexander <warrior0edem@gmail.com>
2022-03-31 23:19:13 +0300

    a kak kakat

--- src/app/interceptors/x-header.interceptor.ts
+++ src/app/interceptors/x-header.interceptor.ts
@@ -12,32 +12,15 @@ export class XHeaderInterceptor implements HttpInterceptor {
 
        intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const newReq = req.clone({
-          headers: req.headers.set('Authorization', `Bearer ${localStorage.getItem('myToken')}`)
+          headers: req.headers.set('Authorization', `Bearer ${localStorage.getItem('myToken')}`),
         });
-    
-        return next.handle(newReq)
-        .pipe(
+        return next.handle(newReq).pipe(
           catchError((err) => {
             if (err.status === 401) {
               this.auth.logout();
-              return this.auth.refreshToken(localStorage.getItem('myRefreshToken')!)
-                .pipe(
-                  switchMap((res: any) => {
-                    localStorage.setItem('myToken', res.token);
-                    return next.handle(
-                      req.clone({
-                        headers: req.headers.set('Authorization', `Bearer ${res.token}`),
-                      })
-                    );
-                  }),
-                  catchError((err) => {
-                    this.auth.logout();
-                    return throwError(err);
-                  })
-                );
+              this.router.navigateByUrl('/login');
             }
-    
-            return throwError(err);
+            return throwError(() => err);
           })
         );
       }
